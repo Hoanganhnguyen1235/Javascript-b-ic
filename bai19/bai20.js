1c337ae8b046cbc555c1fbad9c8fa1f3336dbb67
Hoanganhnguyen1235 <109190054+Hoanganhnguyen1235@users.noreply.github.com>
2022-07-13 14:43:31 +0700

    Update bai20.js

--- bai19/bai20.js
+++ bai19/bai20.js
@@ -20,3 +20,18 @@ console.log(myString.length)
 
 // // 5.Convect to upper case(chuyển đổi chuỗi thành chữ hoa)
 // console.log(myString.toUpperCase())
+// 6.Convert to lower case
+
+   console.log(myString.tolowerCase))
+// 7.Trim
+  console.log(myString.trim())
+
+// 8.split(cắt 1 chuỗi thành 1 arry)
+  var languages = ('Javascript, Php, Ruby');
+  console.log(languages.split(', '))
+//nếu muốn cắt từng chữ cái trong 1 từ ra ta sử dung  
+  console.log(languages.split('')
+
+// 9.Get a character by index(lấy 1 kí tự bởi 1 index cho trước)
+  cont myString = 'Son Dang';
+  console.log(myString.charAt(1))
